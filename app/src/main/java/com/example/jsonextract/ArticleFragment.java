8eddbd4ea20948c4b68801e2e7501318183dbda3
Rohan20s <kansarirohan@gmail.com>
2022-06-14 19:40:07 +0530

    Update ArticleFragment.java

--- app/src/main/java/com/example/jsonextract/ArticleFragment.java
+++ app/src/main/java/com/example/jsonextract/ArticleFragment.java
@@ -45,7 +45,7 @@ public class ArticleFragment extends Fragment {
     }
 
     RecyclerView recyclerView;
-    static ArrayList<Atc> itemList=artcle.atc;
+    static ArrayList<Atc> itemList;
     Button next,prev;
     paginator p=new paginator();
     private int totalPages=paginator.TOTAL_NUM_ITEMS/paginator.ITEMS_PER_PAGE;
